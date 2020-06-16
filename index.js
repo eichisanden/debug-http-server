8e872b44e3a0bfafa3da6f048ae391f158954687
eichi.mita <eichi.mita@rakus.co.jp>
2020-06-16 16:22:43 +0900

    body paserser json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 /*
@@ -15,7 +16,11 @@ express()
 */
 
 app.post('/log', (req, res) => {
+    console.log("request headers");
+    console.log(JSON.stringify(req.headers));
+    console.log("req.body");
     console.log(req.body);
+    req.send(JSON.stringify(req.headers));
     res.send(req.body);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
